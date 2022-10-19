380eadf73f9468effe25a23014d1e2cbee2959f2
Eason <291028775@qq.com>
2022-10-19 14:18:35 +0800

    Create SIWA.vue

---
+++ src/components/SIWA.vue
@@ -0,0 +1,22 @@
+<script setup>
+import DocTem from "../components/DocTem.vue"
+import {ref} from "vue";
+const tree = ref([
+  {
+    title: "快速入门",
+    pane: [
+      {
+        label: "插件介绍",
+        name: 're-siwa'
+      },
+      {
+        label: "参与开发",
+        name: 'dev-siwa'
+      }
+    ]
+  }
+])
+</script>
+<template>
+  <doc-tem path="SIWA" :tree="tree"/>
+</template>
