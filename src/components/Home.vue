790255c2613ef05a1c2585adedd30fc855bb28c1
Eason <291028775@qq.com>
2022-10-28 23:50:16 +0800

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -40,12 +40,12 @@ const statisticsData = [
   {
     tipContent: 'Sonic社区总回复数',
     linkUrl: 'https://sonic-cloud.wiki',
-    img: 'https://img.shields.io/badge/replies%20-10.5k-ff69b4?style=social&logo=LiveChat'
+    img: 'https://img.shields.io/badge/replies%20-10.7k-ff69b4?style=social&logo=LiveChat'
   },
   {
     tipContent: '镜像仓库总下载量',
     linkUrl: 'https://github.com/SonicCloudOrg',
-    img: 'https://img.shields.io/badge/Docker%20pulls%20-58.3k-important'
+    img: 'https://img.shields.io/badge/Docker%20pulls%20-59.1k-important'
   }
 ]
 const news = ref([
