2481cb72bc49de731d6058bd82a41b5aad3c8cda
Eason <291028775@qq.com>
2023-01-15 16:13:15 +0800

    fix: change pur to a tag

--- .vitepress/theme/components/Pur.vue
+++ .vitepress/theme/components/Pur.vue
@@ -1,28 +1,30 @@
 <template>
-  <el-popover
-    placement="top"
-    :width="220"
-    trigger="click"
-    v-if="url.length !== 0"
-  >
-    <el-divider> 购买渠道 </el-divider>
-    <el-link :href="url" target="_blank" type="primary">官方淘宝店</el-link>
-    <el-divider> 咨询渠道 </el-divider>
-    <el-link href="mailto:soniccloudorg@163.com" target="_blank" type="primary"
-      >官方邮箱</el-link
-    >
-    <el-link
-      href="https://sonic-record-hongkong.oss-cn-hongkong.aliyuncs.com/wechat.jpg"
-      target="_blank"
-      type="primary"
-      style="margin-left: 20px"
-      >企微咨询
-    </el-link>
-    <template #reference>
+<!--  <el-popover-->
+<!--    placement="top"-->
+<!--    :width="220"-->
+<!--    trigger="click"-->
+<!--    v-if="url.length !== 0"-->
+<!--  >-->
+<!--    <el-divider> 购买渠道 </el-divider>-->
+<!--    <el-link :href="url" target="_blank" type="primary">官方淘宝店</el-link>-->
+<!--    <el-divider> 咨询渠道 </el-divider>-->
+<!--    <el-link href="mailto:soniccloudorg@163.com" target="_blank" type="primary"-->
+<!--      >官方邮箱</el-link-->
+<!--    >-->
+<!--    <el-link-->
+<!--      href="https://sonic-record-hongkong.oss-cn-hongkong.aliyuncs.com/wechat.jpg"-->
+<!--      target="_blank"-->
+<!--      type="primary"-->
+<!--      style="margin-left: 20px"-->
+<!--      >企微咨询-->
+<!--    </el-link>-->
+<!--    <template #reference>-->
+  <a :href="url" target="_blank">
       <el-button type="primary"> 立即购买 </el-button>
-    </template>
-  </el-popover>
-  <el-button v-else disabled> 即将上架，敬请期待... </el-button>
+  </a>
+<!--    </template>-->
+<!--  </el-popover>-->
+<!--  <el-button v-else disabled> 即将上架，敬请期待... </el-button>-->
 </template>
 
 <script setup>
